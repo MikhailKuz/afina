560336cac9ef5b3cfe4831ec779c8b55a125e4e2
Mikhail <kkuznecov.misha.98@yandex.ru>
2019-10-10 20:55:19 +0300

    Start config

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -17,11 +17,13 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024) :  _max_size(max_size), _current_size(0), _lru_head(nullptr), _lru_tail(nullptr) {}
 
     ~SimpleLRU() {
         _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
+        while (_lru_head != nullptr){
+            _lru_head = std::move(_lru_head->next);
+        }
     }
 
     // Implements Afina::Storage interface
@@ -42,24 +44,35 @@ public:
 private:
     // LRU cache node
     using lru_node = struct lru_node {
-        std::string key;
+        const std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node  *prev;
         std::unique_ptr<lru_node> next;
     };
 
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size;
+    std::size_t _current_size;
 
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
+    lru_node *_lru_tail;
 
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+    std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+
+    bool PutNew(const std::string &key, const std::string &value);
+
+    bool UpdateEl(std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator upt_it, const std::string &value);
+
+    void UpdatePos(lru_node &upt_node);
+
+    bool GetSpace(size_t amount);
+
 };
 
 } // namespace Backend
